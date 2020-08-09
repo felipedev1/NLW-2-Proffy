2d5c9219d318039bbb1cb6a2d5395d664256abf9
Felipe <felipe.pereira.dev@gmail.com>
2020-08-09 10:49:14 -0300

    web - default value select error fix

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -12,8 +12,8 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest}>
-        <option value="" disabled hidden selected>Selecione uma opção</option>
+      <select id={name} {...rest} value="">
+        <option value="" disabled hidden>Selecione uma opção</option>
         {options.map(option => {
           return (
             <option key={option.value} value={option.value}>{option.label}</option>
